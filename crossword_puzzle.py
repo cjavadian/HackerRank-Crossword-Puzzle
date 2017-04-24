f33a1f50f4d61e061e5c08e1799179eff9aa52cc
Catherine Javadian <cjavadia@stevens.edu>
2017-04-24 19:38:08 -0400

    Add files via upload
    Working file for crossword puzzle

---
+++ crossword_puzzle.py
@@ -0,0 +1,24 @@
+# Catherine Javadian, Jennifer Cafiero, and Jordana Approvato
+# We pledge our honor that we have abided by the Stevens Honor System
+
+grid = []
+
+def findBlank():
+    for x in range(10):
+        
+    return
+        
+
+def findSol():
+    findBlank()
+    return
+
+def main():
+    lines = 10
+    while (lines):
+        grid_l = input().strip()
+        grid.append(list(grid_l))
+        lines = lines - 1
+    words = input().split(';')
+    findSol()
+    return
